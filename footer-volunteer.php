5b89b010eed35ea11598f3e95caccb010e4bd28f
Emmy <60683173+emmy-html@users.noreply.github.com>
2021-03-18 04:06:05 -0500

    Added layout for volunteer opportunities page

---
+++ footer-volunteer.php
@@ -0,0 +1,11 @@
+<footer id="volunteer-page-footer">
+      <div class="content-wrapper">
+        <h3>The Meals On Wheels Foundation
+          <span>of Northern Illinois</span>
+          <span>Corporate Volunteering</span>
+        </h3>
+      </div>
+    </footer>
+   <?php wp_footer(); ?>
+   </body>
+   </html>
